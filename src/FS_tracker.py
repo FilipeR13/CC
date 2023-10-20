481a7929ff79bddedecc0e5ab536e04f4eb94796
João Coelho <jhcoelho03@gmail.com>
2023-10-20 19:57:46 +0100

    Added a feature to tracker that makes it possible to register the different nodes

--- src/FS_tracker.py
+++ src/FS_tracker.py
@@ -10,13 +10,19 @@ class fs_tracker():
         self.server_socket.bind((self.host, self.port))
         self.server_socket.listen(5)
         self.node_threads = {}
+        self.nodes = {}
 
     def handle_client(self, socket_node):
         while True:
             data = socket_node.recv(1024)
             if not data:
                 break
-            print(f"Data recebida pela porta {socket_node.getpeername()[1] }: {data.decode()}")
+            if data.decode()  == "Current nodes":
+                print("Lista de nodes atualmente conectados:")
+                for port, node in self.nodes.items():
+                    print("[" + f"Porta -> {node['port']}" + "," + f"Host -> {node['host']}" + "]")
+            else:
+                print(f"Data recebida pela porta {socket_node.getpeername()[1] }: {data.decode()}")
         socket_node.close()
 
     def start_connections(self):
@@ -31,6 +37,11 @@ class fs_tracker():
             thread_node = threading.Thread(target=self.handle_client, args=(socket_node,))
             self.node_threads[porta_node] = thread_node
             thread_node.start()
+
+            self.nodes[porta_node] = {
+                'host': host_node,
+                'port': porta_node
+            }
 
 
 if __name__ == "__main__":
