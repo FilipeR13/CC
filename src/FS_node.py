1f9129167d014a93ad4f46135c6622ff4a32afac
João Coelho <jhcoelho03@gmail.com>
2023-10-20 21:19:25 +0100

    Changed the way a node is created

--- src/FS_node.py
+++ src/FS_node.py
@@ -1,27 +1,31 @@
 import socket
+import sys
 
 
 class fs_node():
     def __init__(self, host, port):
         self.host = host
-        self.port = port
+        self.port = int(port)
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.client_socket.connect(('localhost',9090))
+        self.client_socket.connect((self.host,self.port))
         print("Coneção FS Track Protocol com servidor localhost porta 9090")
 
     def send_data(self, data):
         self.client_socket.send(data.encode())
-
-if __name__ == "__main__":
-    tracker_host = 'localhost'
-    tracker_port = 9090
-    node = fs_node('localhost', 54321)
-
-    # Enter a loop to read input from the terminal and send it to the tracker
+    
+def main():
+    if len(sys.argv) != 4:
+        print("Erro nos argumentos: FS_node.py <message> <host> <port>")
+    node = fs_node(sys.argv[2],sys.argv[3])
     while True:
         user_input = input("Enter a message: ")
         if user_input:
             node.send_data(user_input)
 
+
+
+
+if __name__ == "__main__":
+    main()
     # Ver multithreading em tcp: a porta do server recebe um pedido do cliente e depois cria uma nova thread que criará uma nova porta exclusiva para esse cliente.
     # Assim podemos evitar que quando um cliente esteja a utilizar a porta do server os outros tenham de ficar à espera.
