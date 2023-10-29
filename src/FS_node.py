3fba40043252d23e50741d5755846062c21652d8
João Coelho <jhcoelho03@gmail.com>
2023-10-29 19:38:40 +0000

    feat: multi threading fully working

--- src/FS_node.py
+++ src/FS_node.py
@@ -10,12 +10,30 @@ class fs_node():
         self.path = path
         self.host = host
         self.port = int(port)
+        self.exlusive_connection = None
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.client_socket.connect((self.host,self.port))
         print("Coneção FS Track Protocol com servidor localhost porta 9090")
 
     def send_data(self, data):
-        self.client_socket.send(data.encode())
+        self.exlusive_connection.send(data.encode())
+
+
+    def receive_new_port(self):
+        response = self.client_socket.recv(1024)
+        if response:
+            length, message_type, payload = struct.unpack(f'!IB{len(response) - 5}s', response)
+            if message_type == 0x03:
+                new_port = int(payload.decode('utf-8'))
+                print(f"Received new port: {new_port}")
+                return new_port
+        return None
+    
+    def create_new_connection(self, new_port):
+
+        self.exlusive_connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.exlusive_connection.connect((self.host, new_port))
+        print(f"Conexão FS Track Protocol com servidor localhost porta {new_port}")
 
     def send_name_files(self):
         files = [f.encode('utf-8') for f in os.listdir() if os.path.isfile(f)]
@@ -25,7 +43,7 @@ class fs_node():
 
         # Pack the list of encoded strings into a struct
         packet = Message(0x01, b' '.join(files), f'!IB{total_length}s').create_struct_message()
-        self.client_socket.send(packet)
+        self.exlusive_connection.send(packet)
         # chunks = [packet[i:i + 1024] for i in range(0, len(packet), 1024)]
 
         # Send the chunks
@@ -37,7 +55,14 @@ class fs_node():
 def main():
     if len(sys.argv) != 4:
         print("Erro nos argumentos: FS_node.py <path> <host> <port>")
+
     node = fs_node(sys.argv[1],sys.argv[2],sys.argv[3])
+
+    new_port = node.receive_new_port()
+
+    if new_port is not None:
+        node.create_new_connection(new_port)
+
     node.send_name_files()
     try:
         while True:
--- src/FS_tracker.py
+++ src/FS_tracker.py
@@ -14,6 +14,16 @@ class fs_tracker():
         self.node_threads = {}
         self.nodes = {}
 
+    def find_available_port(self,host, starting_port, attempts=100):
+        for port in range(starting_port, starting_port + attempts):
+            try:
+                with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as new_socket:
+                    new_socket.bind((host, port))
+                    return port
+            except OSError:
+                pass
+        return None
+
     def handle_wake_up(self, socket_node, payload):
         files =  [file.decode('utf-8') for file in payload.split(b' ')]
         print(files)
@@ -22,6 +32,7 @@ class fs_tracker():
     def handle_request(self, socket_node, payload):
         print("Request")
         print(payload)
+
     
     def handle_response(self, socket_node, payload):
         print("Response")
@@ -29,32 +40,36 @@ class fs_tracker():
 
     def close_client(self, socket_node):
         print(f"Node {socket_node.getpeername()[1]} desconectado")
-        del self.node_threads[socket_node.getpeername()[1]]
-        del self.nodes[socket_node.getpeername()[1]]
+        port = socket_node.getpeername()[1]
+        if port in self.node_threads:
+            self.node_threads[port].join()
+            del self.node_threads[port]
+        del self.nodes[port]
         socket_node.close()
 
-
-    def handle_client(self, socket_node):
-        handle_flags =  {
-            WAKE_UP: self.handle_wake_up,
-            REQUEST: self.handle_request,
-            RESPONSE: self.handle_response
-        }
-
+    def handle_client(self, socket_node, connection_event):
         while True:
+            connection_event.wait()
+
             data = socket_node.recv(PACKET_SIZE)
             if not data:
                 break
             data_decoded = data.decode()
 
-            if data_decoded  == "Current nodes":
+            if data_decoded == "Current nodes":
                 print("Lista de nodes atualmente conectados:")
                 for port, node in self.nodes.items():
-                    print("[" + f"Porta -> {port}" + "," + f"Host -> {node['host']}" + f", Files -> {node['files']}" +"]")
+                    print("[" + f"Porta -> {port}" + "," + f"Host -> {node['host']}" + f", Files -> {node['files']}" + "]")
             else:
                 length, message_type, payload = struct.unpack(f'!IB{len(data) - 5}s', data)
-                print(f"Data recebida pela porta {socket_node.getpeername()[1] }: {length} || {message_type} || {payload}")
-                handle_flags[message_type](socket_node, payload)
+                print(f"Data recebida pela porta {socket_node.getpeername()[1]}: {length} || {message_type} || {payload}")
+
+                if message_type == WAKE_UP:
+                    self.handle_wake_up(socket_node, payload)
+                elif message_type == REQUEST:
+                    self.handle_request(socket_node, payload)
+                elif message_type == RESPONSE:
+                    self.handle_response(socket_node, payload)
 
         self.close_client(socket_node)
 
@@ -68,14 +83,33 @@ class fs_tracker():
                 host_node, porta_node = socket_node.getpeername()
                 print(f"Node conectado a partir de {host_node} na porta {porta_node}")
 
-                thread_node = threading.Thread(target=self.handle_client, args=(socket_node,))
-                self.node_threads[porta_node] = thread_node
-                thread_node.start()
+                new_port = self.find_available_port(self.host, self.port + 1)
+                if new_port is not None:
+                    
+                    new_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                    new_socket.bind((self.host, new_port))
+                    new_socket.listen(1)
 
-                self.nodes[porta_node] = {
-                    'host': host_node,
-                    'files': []
-                }
+                    connection_event = threading.Event()
+
+                    client_thread = threading.Thread(target=self.handle_client, args=(new_socket,connection_event))
+                    client_thread.start()
+
+                    response = str(new_port).encode('utf-8')
+                    response_message = Message(RESPONSE, response, f'!IB{len(response)}s').create_struct_message()
+                    socket_node.send(response_message)
+
+
+                else:
+                    response = "No available port".encode('utf-8')
+                    response_message = Message(RESPONSE, response, f'!IB{len(response)}s').create_struct_message()
+                    socket_node.send(response_message)
+
+                    self.nodes[new_port] = {
+                        'host': host_node,
+                        'files': []
+                    }
+                
         except KeyboardInterrupt:
             print("Keyboard Interrupt")
             self.server_socket.close()
